8ed5d3e187fc24e27ecf2932267ddeadf194f376
sankeerthreddy1597 <54692080+sankeerthreddy1597@users.noreply.github.com>
2021-01-06 13:32:14 -0700

    neumorphism feature- footer

---
+++ scripts.js
@@ -0,0 +1,10 @@
+//function to select element
+const selectElement = (s) => document.querySelector(s);
+//open menu on click
+selectElement('.open').addEventListener('click', () => {
+    selectElement('.nav-list').classList.add('active')
+});
+//close menu on click
+selectElement('.close').addEventListener('click', () => {
+    selectElement('.nav-list').classList.remove('active')
+});
